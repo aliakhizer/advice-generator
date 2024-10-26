1b2be7d073e8c72f8b4b56512c5900268d16070a
Alia Khizer <aliakhizar6@gmail.com>
2024-10-26 23:37:46 +0500

    basic project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Card } from "./components/Card";
+
+function App() {
+  return (
+    <main className="main">
+      <Card />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+export const Card = () => {
+  return (
+    <div className="card">
+      <h1>hello</h1>
+    </div>
+  );
+};
